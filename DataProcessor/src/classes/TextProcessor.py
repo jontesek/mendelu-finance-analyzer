26f99f0bb2e37a4b16c419c9ef8bfbc4b9aae2fe
Jonas Petrovsky <jond@post.cz>
2015-10-26 12:49:40 +0100

    price movements added and text processing started

--- DataProcessor/src/classes/TextProcessor.py
+++ DataProcessor/src/classes/TextProcessor.py
@@ -9,7 +9,7 @@ class TextProcessor(object):
     def __init__(self, file_paths):
         self.dbmodel = TextProcessorDbModel()
         #self.stop_words = self._read_stopwords(file_paths['stopwords'])
-        self.stock_movements = {'company_id': None, 'prices': {}}
+        self.stock_movements = {'company_id': None, 'ratios': {}}
 
     # MAIN PROCESSING
 
@@ -17,30 +17,33 @@ class TextProcessor(object):
         articles = self.dbmodel.get_articles_for_company(company_id)
         print articles.fetchone()
 
-    def process_fb_posts_for_company(self, company_id, from_date):
+    def process_fb_posts_for_company(self, company_id, from_date, days_delay):
         fb_posts = self.dbmodel.get_fb_posts_for_company(company_id, from_date)
         new_post_list = []
         for post in fb_posts:
             print post['id'],
+            # Get price movement
             post_date = datetime.datetime.utcfromtimestamp(post['created_timestamp']).date()
-            lookup_date = post_date + datetime.timedelta(days=1)
-            print lookup_date,
-            price_movement = self.stock_movements['prices'][lookup_date]
-            print price_movement
-            pass# Convert timestamp to date and text to lowercase
-            # Remove stop words
+            lookup_date = post_date + datetime.timedelta(days=days_delay)
+            working_date = self._get_working_date(lookup_date)
+            print lookup_date, working_date,
+            price_movement = self.stock_movements['ratios'][working_date]
+            print price_movement,
+            # Edit text
+            post_text = self._process_facebook_text(post['text'])
+            print post_text
 
-    def get_stock_movements(self, company_id, from_date):
+    def set_stock_movements(self, company_id, from_date):
         """Get relative stock movement for days from given date to present day.
 
         Args:
             company_id (int): Company ID
-            from_date (Datetime): from which date to search (also two previous days will be saved)
+            from_date (Datetime): from which date to search (also 3 previous days will be saved)
 
         Returns:
             list: (datetime, float): stock price movements as percentage change (current/last day)
         """
-        # Substract three days to be sure to get data for the from_date.
+        # Substract 3 days to be sure to get data for the from_date.
         early_from_date = from_date - datetime.timedelta(days=3)
         # Get stock prices for individual dates
         stock_prices = self.dbmodel.get_stock_prices(company_id, early_from_date)
@@ -49,10 +52,40 @@ class TextProcessor(object):
             if index == 0:
                 continue
             ratio = (price / stock_prices[index - 1][1]) - 1
-            self.stock_movements['prices'][price_date] = ratio*100
+            self.stock_movements['ratios'][price_date] = ratio*100
         # return list
         self.stock_movements['company_id'] = company_id
         print self.stock_movements
+
+    def _get_working_date(self, lookup_date):
+        """Gheck if given date is a working day. If not, return minus one or minus two days date.
+
+        Args:
+            lookup_date (Datetime)
+
+        Returns:
+            Datetime: the same date or date of the previous working day
+        """
+        if lookup_date in self.stock_movements['ratios']:
+            return lookup_date
+        date_minus_1 = lookup_date - datetime.timedelta(days=1)
+        if date_minus_1 in self.stock_movements['ratios']:
+            return date_minus_1
+        date_minus_2 = lookup_date - datetime.timedelta(days=2)
+        if date_minus_2 in self.stock_movements['ratios']:
+            return date_minus_2
+
+
+    def _process_facebook_text(self, text):
+        # Remove whitespace
+        text = ' '.join(text.strip().split())
+        # Remove hyper links - regexp
+        
+        # Lowercase the text
+        #text = unicode(text,'utf-8').lower()
+        text = text.lower()
+
+        return text
 
 
 
--- DataProcessor/src/test.py
+++ DataProcessor/src/test.py
@@ -4,8 +4,8 @@ from classes.TextProcessor import TextProcessor
 
 file_paths = {'stopwords': '../input_sources/google_en_stopwords.txt'}
 tp = TextProcessor(file_paths)
-tp.get_stock_movements(1,datetime(2015,9,19))
-tp.process_fb_posts_for_company(1,datetime(2015,9,20))
+tp.set_stock_movements(1,datetime(2015,9,19))
+tp.process_fb_posts_for_company(1,datetime(2015,9,20),1)
 #tp.remove_stop_words('A zdar, from this shit')
 #tp.process_fb_posts_for_company(1)
 
